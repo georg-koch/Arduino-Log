38c412d53b5f76e9047cce9f554b8410dfbc54e1
Georg Koch <dev@bytemania.eu>
2018-11-02 09:42:12 +0100

    format documentation

--- ArduinoLog.h
+++ ArduinoLog.h
@@ -13,13 +13,6 @@
 #endif
 typedef void (*printfunction)(Print *);
 
-//#include <stdint.h>
-//#include <stddef.h>
-// *************************************************************************
-//  Uncomment line below to fully disable logging, and reduce project size
-// ************************************************************************
-//#define DISABLE_LOGGING
-
 #define LOG_LEVEL_SILENT 0
 #define LOG_LEVEL_FATAL 1
 #define LOG_LEVEL_ERROR 2
@@ -29,42 +22,34 @@ typedef void (*printfunction)(Print *);
 #define LOG_LEVEL_VERBOSE 6
 
 #define CR "\n"
-#define LOGGING_VERSION 1_0_0
 
-/*!
- Logging is a helper class to output informations over
- RS232. If you know log4j or log4net, this logging class
- is more or less similar ;-) <br>
+/*
+ Logging is a helper class to output informations over RS232. 
  Different loglevels can be used to extend or reduce output
  All methods are able to handle any number of output parameters.
  All methods print out a formated string (like printf).<br>
  To reduce output and program size, reduce loglevel.
 
- Output format string can contain below wildcards. Every wildcard
- must be start with percent sign (\%)
+ Output format string can contain wildcards:
+ - %s	replace with an string (char*)
+ - %c	replace with an character
+ - %d	replace with an integer value
+ - %l	replace with an long value
+ - %x	replace and convert integer value into hex
+ - %X	like %x but combine with 0x123AB
+ - %b	replace and convert integer value into binary
+ - %B	like %x but combine with 0b10100011
+ - %t	replace and convert boolean value into "t" or "f"
+ - %T	like %t but convert into "true" or "false"
 
-**** Wildcards
-
-* %s	replace with an string (char*)
-* %c	replace with an character
-* %d	replace with an integer value
-* %l	replace with an long value
-* %x	replace and convert integer value into hex
-* %X	like %x but combine with 0x123AB
-* %b	replace and convert integer value into binary
-* %B	like %x but combine with 0b10100011
-* %t	replace and convert boolean value into "t" or "f"
-* %T	like %t but convert into "true" or "false"
-
-**** Loglevels
-
-* 0 - LOG_LEVEL_SILENT     no output
-* 1 - LOG_LEVEL_FATAL      fatal errors
-* 2 - LOG_LEVEL_ERROR      all errors
-* 3 - LOG_LEVEL_WARNING    errors and warnings
-* 4 - LOG_LEVEL_DEBUG      errors, warnings and debug
-* 5 - LOG_LEVEL_TRACE      errors, warnings, debug, traces
-* 6 - LOG_LEVEL_VERBOSE    all
+ Supported Loglevels:
+ - LOG_LEVEL_SILENT   0   no output
+ - LOG_LEVEL_FATAL    1   fatal errors
+ - LOG_LEVEL_ERROR    2   all errors
+ - LOG_LEVEL_WARNING  3   errors and warnings
+ - LOG_LEVEL_DEBUG    4   errors, warnings and debug
+ - LOG_LEVEL_TRACE    5   errors, warnings, debug, traces
+ - LOG_LEVEL_VERBOSE  6   all
 */
 
 class Logging
@@ -93,13 +78,13 @@ class Logging
 	void init(int level, long baud);
 
 	/**
-    * Initializing, must be called as first. Note that if you use
-    * this variant of Init, you need to initialize the baud rate
-    * yourself, if printer happens to be a serial port.
-    * @param level - logging levels <= this will be logged.
-    * @param printer - place that logging output will be sent to.
-    * @return void
-    */
+        * Initializing, must be called as first. Note that if you use
+        * this variant of Init, you need to initialize the baud rate
+        * yourself, if printer happens to be a serial port.
+        * @param level - logging levels <= this will be logged.
+        * @param printer - place that logging output will be sent to.
+        * @return void
+        */
 	void init(int level, Print *output, bool showLevel = true);
 
 	/**
@@ -147,6 +132,7 @@ class Logging
 		printLevel(LOG_LEVEL_ERROR, msg, args...);
 #endif
 	}
+	
 	/**
 	* Output a warning message. Output message contains
 	* W: followed by original message
@@ -157,7 +143,6 @@ class Logging
 	* @param ... any number of variables
 	* @return void
 	*/
-
 	template <class T, typename... Args>
 	void warning(T msg, Args... args)
 	{
@@ -165,6 +150,7 @@ class Logging
 		printLevel(LOG_LEVEL_WARNING, msg, args...);
 #endif
 	}
+	
 	/**
 	* Output a debug message. Output message contains
 	* N: followed by original message
@@ -175,7 +161,6 @@ class Logging
 	* @param ... any number of variables
 	* @return void
 	*/
-
 	template <class T, typename... Args>
 	void debug(T msg, Args... args)
 	{
@@ -183,6 +168,7 @@ class Logging
 		printLevel(LOG_LEVEL_DEBUG, msg, args...);
 #endif
 	}
+	
 	/**
 	* Output a trace message. Output message contains
 	* N: followed by original message
